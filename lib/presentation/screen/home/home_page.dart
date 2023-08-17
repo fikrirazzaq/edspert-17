54d15301eaec1949a8cfa16b689e73f7111ba01b
adithamafadil <dil.projek@gmail.com>
2023-08-17 21:54:09 +0700

    Update

--- lib/presentation/screen/home/home_page.dart
+++ lib/presentation/screen/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learning/presentation/screen/home/home_controller.dart';
 import 'package:learning/presentation/screen/home/widgets/banner_builder.dart';
+import 'package:learning/presentation/screen/home/widgets/course_builder.dart';
 import 'package:learning/presentation/screen/home/widgets/welcoming_widget.dart';
 import 'package:learning/presentation/screen/widgets/section_title.dart';
 
@@ -76,65 +77,7 @@ class MyHomePage extends StatelessWidget {
 
                     return courseList.isEmpty
                         ? const Center(child: CircularProgressIndicator())
-                        : ListView.separated(
-                            itemCount: 3,
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            separatorBuilder: (context, index) =>
-                                const SizedBox(height: 8),
-                            itemBuilder: (context, index) {
-                              final course = cHome.courseList[index];
-
-                              return Container(
-                                height: 96,
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 18),
-                                width: Get.width,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Row(
-                                  children: [
-                                    Container(
-                                      height: 53,
-                                      width: 53,
-                                      decoration: BoxDecoration(
-                                        color: const Color(0xFFF3F7F8),
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                    ),
-                                    const SizedBox(width: 16),
-                                    Expanded(
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            course.majorName,
-                                            style: const TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                          Text(
-                                            '${course.jumlahDone} / ${course.jumlahMateri} latihan soal',
-                                            style: const TextStyle(
-                                                color: Colors.grey),
-                                          ),
-                                          const SizedBox(height: 11),
-                                          const LinearProgressIndicator(
-                                            value: 0.5,
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              );
-                            },
-                          );
+                        : CourseBuilder(courseList: courseList);
                   }),
             ],
           ),
